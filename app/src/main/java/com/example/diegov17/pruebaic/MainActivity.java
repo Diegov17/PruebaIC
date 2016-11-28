5b44f10e7112a8f8adc20976cc4cb119b1d4700b
Diego Vierma <diegovierma88@gmail.com>
2016-11-28 13:13:41 +0100

    Prueba con crash

--- app/src/main/java/com/example/diegov17/pruebaic/MainActivity.java
+++ app/src/main/java/com/example/diegov17/pruebaic/MainActivity.java
@@ -2,15 +2,32 @@ package com.example.diegov17.pruebaic;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button boton;
+    private TextView textview;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        textview = (TextView) findViewById(R.id.textview);
 
+        textview = null;
+
+        boton = (Button) findViewById(R.id.botonPrueba);
+
+        boton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                textview.setText("prueba");
+            }
+        });
 
     }
 }
